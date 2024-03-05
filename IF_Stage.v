7c40a39dc9ec078f32f663cb06c09e0616f8933f
Salar Safardoust <salar.sfd@gmail.com>
2024-03-05 14:39:10 +0330

    IF files

---
+++ IF_Stage.v
@@ -0,0 +1,7 @@
+module IF_Stage(
+    input clk, rst, freeze, branchTaken,
+    input[31:0] branchAddress,
+    output[31:0] pc, instruction);
+
+    
+endmodule
---
+++ IF_Stage_Reg.v
@@ -0,0 +1,6 @@
+module IF_Stage_Reg(
+    input clk, rst, freeze, flush,
+    input[31:0] pcIn, instructionIn,
+    output reg[31:0] pc, instruction);
+    
+endmodule
