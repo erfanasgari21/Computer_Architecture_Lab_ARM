99f5a46ca6fc177e9168b2779a46a48b1acae1c2
rfan sgari <erfanasgari21@gmail.com>
2024-04-09 13:28:48 +0330

    ID top level

--- Control_Unit.v
+++ Control_Unit.v
@@ -4,35 +4,35 @@ module Control_Unit(
     input sIn,
     output[8:0] controlOut
 );
-    reg[3:0] exeCmd;
-    reg memReadEn, memWriteEn, writeBackEn, b, sOut;
+    reg[3:0] exeCmd_ID;
+    reg memReadEn_ID, memWriteEn_ID, writeBackEn, b, sOut;
 
     always @(mode or opCode or sIn) begin
-        {exeCmd, memReadEn, memWriteEn, writeBackEn, b, sOut} = 9'b0;
+        {exeCmd_ID, memReadEn_ID, memWriteEn_ID, writeBackEn, b, sOut} = 9'b0;
         case(mode)
             2'b00: begin
                 sOut <= sIn;
                 case(opCode)
-                    4'b1101: {exeCmd, writeBackEn} <= {4'b0001, 1'b1}; //MOVE
-                    4'b1111: {exeCmd, writeBackEn} <= {4'b1001, 1'b1}; //MVN
-                    4'b0100: {exeCmd, writeBackEn} <= {4'b0010, 1'b1}; //ADD
-                    4'b0101: {exeCmd, writeBackEn} <= {4'b0011, 1'b1}; //ADC
-                    4'b0010: {exeCmd, writeBackEn} <= {4'b0100, 1'b1}; //SUB
-                    4'b0110: {exeCmd, writeBackEn} <= {4'b0101, 1'b1}; //SBC
-                    4'b0000: {exeCmd, writeBackEn} <= {4'b0110, 1'b1}; //AND
-                    4'b1100: {exeCmd, writeBackEn} <= {4'b0111, 1'b1}; //ORR
-                    4'b0001: {exeCmd, writeBackEn} <= {4'b1000, 1'b1}; //EOR
-                    4'b1010: exeCmd <= 4'b0100;                        //CMP
-                    4'b1000: exeCmd <= 4'b0110;                        //TST
+                    4'b1101: {exeCmd_ID, writeBackEn} <= {4'b0001, 1'b1}; //MOVE
+                    4'b1111: {exeCmd_ID, writeBackEn} <= {4'b1001, 1'b1}; //MVN
+                    4'b0100: {exeCmd_ID, writeBackEn} <= {4'b0010, 1'b1}; //ADD
+                    4'b0101: {exeCmd_ID, writeBackEn} <= {4'b0011, 1'b1}; //ADC
+                    4'b0010: {exeCmd_ID, writeBackEn} <= {4'b0100, 1'b1}; //SUB
+                    4'b0110: {exeCmd_ID, writeBackEn} <= {4'b0101, 1'b1}; //SBC
+                    4'b0000: {exeCmd_ID, writeBackEn} <= {4'b0110, 1'b1}; //AND
+                    4'b1100: {exeCmd_ID, writeBackEn} <= {4'b0111, 1'b1}; //ORR
+                    4'b0001: {exeCmd_ID, writeBackEn} <= {4'b1000, 1'b1}; //EOR
+                    4'b1010: exeCmd_ID <= 4'b0100;                        //CMP
+                    4'b1000: exeCmd_ID <= 4'b0110;                        //TST
                 endcase
             end
 
             2'b01: begin
-                exeCmd <= 4'b0010;
+                exeCmd_ID <= 4'b0010;
                 sOut <= 1'b1;
                 case(sIn)
-                    1'b1: {memReadEn, writeBackEn} <= {1'b1, 1'b1}; //LDR
-                    1'b0: memWriteEn <= 1'b1;                       //STR
+                    1'b1: {memReadEn_ID, writeBackEn} <= {1'b1, 1'b1}; //LDR
+                    1'b0: memWriteEn_ID <= 1'b1;                       //STR
                 endcase
             end
             
@@ -41,5 +41,5 @@ module Control_Unit(
 
         endcase
     end        
-    assign controlOut = {exeCmd, memReadEn, memWriteEn, writeBackEn, b, sOut};
+    assign controlOut = {exeCmd_ID, memReadEn_ID, memWriteEn_ID, writeBackEn, b, sOut};
 endmodule
--- Top_Level.v
+++ Top_Level.v
@@ -1,12 +1,23 @@
 module Top_Level(
     input clk, rst
 );
-    wire [31:0] pc_IF, pc_IF_Reg, pc_ID, pc_ID_Reg, pc_EXE, pc_EXE_Reg, pc_MEM, pc_MEM_Reg, pc_WB, pc_WB_Reg;
+    wire [31:0] pc_IF, pc_IF_Reg, pc_ID_Reg, pc_EXE, pc_EXE_Reg, pc_MEM, pc_MEM_Reg, pc_WB, pc_WB_Reg;
     wire [31:0] inst_IF, inst_IF_Reg;
+
+    // ID Stage
+    wire [31:0] valRn_ID, valRm_ID;
+    wire [23:0] signedimm_ID24_ID;
+    wire [11:0] shiftOperand_ID;
+    wire [3:0] exeCmd_ID, destID_ID;
+    wire imm_ID, writeBackEn_ID, memReadEn_ID, memWriteEn_ID, b_ID, s_ID;
+    wire [3:0] src1_ID, src2_ID;
+    wire twoSrc_ID;
+    // ID Stage Reg
+
     IF_Stage        IF(clk, rst, 1'b0, 1'b0, 32'b0, pc_IF, inst_IF);
     IF_Stage_Reg    IF_Reg(clk, rst, 1'b0, 1'b0, pc_IF, inst_IF, pc_IF_Reg, inst_IF_Reg);
-    ID_Stage        ID(clk, rst, pc_IF_Reg, pc_ID);
-    ID_Stage_Reg    ID_Reg(clk, rst, pc_ID, pc_ID_Reg);
+    ID_Stage        ID(clk, rst, inst_IF_Reg, 32'b0, 4'b0, 1'b0, 1'b0, 4'b0,  valRn_ID, valRm_ID, signedimm_ID24_ID, shiftOperand_ID,exeCmd_ID, destID_ID, imm_ID, writeBackEn_ID, memReadEn_ID, memWriteEn_ID, b_ID, s_ID, src1_ID, src2_ID,  twoSrc_ID);
+    ID_Stage_Reg    ID_Reg(clk, rst, );
     // EXE_Stage       EXE(clk, rst, pc_ID_Reg, pc_EXE);
     // EXE_Stage_Reg   EXE_Reg(clk, rst, pc_EXE, pc_EXE_Reg);
     // MEM_Stage       MEM(clk, rst, pc_EXE_Reg, pc_MEM);
