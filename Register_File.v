4601d3727e47a291f809c952e5aeab182eb85d7e
Salar Safardoust <salar.sfd@gmail.com>
2024-04-09 15:09:29 +0330

    ID_Stage

--- Register_File.v
+++ Register_File.v
@@ -6,21 +6,21 @@ module Register_File(
     output[31:0] reg1, reg2
 );
     integer i;
-    reg[15:0] registerFile[14:0];
+    reg[31:0] registerFile[14:0];
     
     assign reg1 = registerFile[src1];
     assign reg2 = registerFile[src2];
 
     initial begin
-        for(i=0; i<14; i=i+1) begin
+        for(i=0; i<15; i=i+1) begin
                 registerFile[i] <= i;
         end
     end
     
     always @(negedge clk or posedge rst) begin
         if(rst) begin
-            for(i=0; i<14; i=i+1) begin
-                registerFile[i] <= 16'b0;
+            for(i=0; i<15; i=i+1) begin
+                registerFile[i] <= i;
             end
         end
         else if(writeBackEn) begin
