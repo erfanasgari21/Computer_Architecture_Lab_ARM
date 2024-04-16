16cda9ca21e01d54da356dead577e38e8a2b8d49
rfan sgari <erfanasgari21@gmail.com>
2024-04-16 14:26:39 +0330

    WB Stage Ready

--- WB_Stage.v
+++ WB_Stage.v
@@ -2,7 +2,8 @@ module WB_Stage(
     input clk, rst,
     input [31:0] dstIn, memResult, aluResult,
     input memReadEn, wbEnIn
-    output [31:0] wbDst, wbValue, wbEn
+    output [31:0] wbDst, wbValue,
+    output wbEn
 );
     assign wbDst = dstIn;
     assign wbEn = wbEnIn;
