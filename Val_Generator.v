708541f53ee358d78392b5b07266aeeccdd6039b
rfan sgari <erfanasgari21@gmail.com>
2024-04-16 13:57:21 +0330

    val generator edited

--- Val_Generator.v
+++ Val_Generator.v
@@ -1,23 +1,25 @@
 module Val_Generator(
     input [31:0] valRm,
-    input imm,
+    input imm, isMem
     input [11:0] shiftOperand,
-    input [23:0] signedImm24,
-
     output [31:0] valOut,
 );
 
     always@(valRm, imm, shiftOperand, signedImm24)
     begin
-        if(imm) begin
+        valOut = 32'b0;
+        if(isMem) begin
+            valOut = shiftOperand;
+        end
+        else if(imm) begin
             valOut = (shiftOperand[7:0] >> shiftOperand[11:8]) | (shiftOperand[7:0] << (32-shiftOperand[11:8]));
         end
         else if(~shiftOperand[4]) begin
             case(shiftOperand[6:5])
-                2'b00: valRm << shiftOperand[11:7];
-                2'b01: valRm >> shiftOperand[11:7];
-                2'b10: //
-                2'b11: //
+                2'b00: valOut = valRm << shiftOperand[11:7];
+                2'b01: valOut = valRm >> shiftOperand[11:7];
+                2'b10: valOut = valRm >>> shiftOperand[11:7];
+                2'b11: valOut = (valRm >> shiftOperand[11:7]) | (valRm << (32-shiftOperand[11:7]))
             endcase
         end
     end
