d2e856f39c666e3cb79a62e93a8a0d511a42cd99
Salar Safardoust <salar.sfd@gmail.com>
2024-05-28 13:58:57 +0330

    j

--- Top_Level_TB.v
+++ Top_Level_TB.v
@@ -1,9 +1,11 @@
 `timescale 1ns/1ns
 module Top_Level_TB();
     reg clk, rst, forwardingEn;
-    
+    wire [15:0]sramData;
+    wire [17:0]sramAddress;
+    wire [4:0]sramCtrl;
 
-    Top_Level CUT(clk, rst, forwardingEn);
+    Top_Level CUT(clk, rst, forwardingEn, sramData, sramAddress, sramCtrl);
     initial begin
         clk = 0;
         forwardingEn = 0;
