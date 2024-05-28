0e8bb2705e855461a58d4f95649ca4b949b33108
rfan sgari <erfanasgari21@gmail.com>
2024-05-28 14:52:49 +0330

    SRAM

--- SRAM.v
+++ SRAM.v
@@ -1,21 +1,22 @@
 
 module SRAM(
-    input clk, rst, memRead, memWrite,
+    input clk, rst, memWrite_n,
     input [17:0] address, 
     inout [15:0] data,
-    output [15:0] memResult
 );
     integer i=0;
-    reg[31:0] memory[0:63];
-    wire[31:0] temp;
+    reg [15:0] memory[0:127];
+    wire[15:0] temp;
 
     initial begin
-        for(i=0; i<64; i=i+1) begin
+        for(i=0; i<128; i=i+1) begin
                 memory[i] <= 0;
         end
     end
-    assign memResult = memRead ? memory[(address-32'd1024)>>2]: 32'b0;
+
+    assign data = memWrite_n ? memory[address] : 16'bz;
+    
     always @(posedge clk) begin
-        if(memWrite) memory[(address-1024)>>2] <= data;
+        if(~memWrite_n) memory[address] <= data;
     end
 endmodule
